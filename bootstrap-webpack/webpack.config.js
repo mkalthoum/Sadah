9d8abf61ea7a1d6c0a60d03f48e20e670cf07edb
RIGHTPOINT\BSewards <bsewards@rightpoint.com>
2018-07-05 17:02:22 -0400

    un-tracked node_modules and added gitignore

--- bootstrap-webpack/webpack.config.js
+++ bootstrap-webpack/webpack.config.js
@@ -34,7 +34,9 @@ module.exports = {
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
+            loader: 'sass-loader', options: {
+              sourceMap: true
+            }
           }
         ]
       }
