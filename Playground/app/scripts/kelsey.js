c36173b4e1561bd4f896d1631a7b97377f851f4e
Muhammad Kalthoum <muhammad.kalthoum@gmail.com>
2018-08-23 14:40:22 -0400

    Lots of changes! - Create a universal nav across all pages. - Boilerplate - HTML 5 implemented - Blog page main scc - Bolierplate page main scss - Boilerplate page created - Styling of mobile responsive for blog posts on boilerplate page - clean up

--- Playground/app/scripts/kelsey.js
+++ Playground/app/scripts/kelsey.js
@@ -1,20 +1,42 @@
-console.log('#image');
-
 $(document).ready(function() {
-    $(document).on('click','#clickme',function(){
-      $("#image").playKeyframe({
 
-        name: 'rotateRight',
-        animation:'01s rotateRight infinite linear',
-        iterationCount: 'infinite',
-      transform-origin:'50% 50%',
-        complete: increment,
+  // Setup the initial transform style.
 
-      });
 
+  // Adding a new animation sequences (keyframe)
+  $.keyframe.define([{
+    name: 'ball-spin',
+    from: {
+      'transform': 'rotate(0deg)'
+    },
+    to: {
+      'transform': 'rotate(350deg)'
+    }
+  }]);
 });
-$.keyframe.define([{
-  name: 'rotateRight',
-  '100%': {transform: 'rotate(360deg)'},
 
-}]);
+
+function play(animation) {
+  $('.ball').resetKeyframe(function() {
+    switch (animation) {
+      case 'spin':
+
+        // run spin keyframes using the shorthand method.
+        $('.ball').playKeyframe('ball-spin 150ms linear  0s infinite');
+
+        break;
+
+    }
+  })
+
+}
+
+function pause() {
+  // freeze keyframe animation and kill callback
+  $('.ball').pauseKeyframe();
+}
+
+function resume() {
+  // resume keyframe animation
+  $('.ball').resumeKeyframe();
+}
